bf2f2112f4a85c2722685dcbde3670f09b948c09
daep996 <daep996@gmail.com>
2025-04-10 00:19:53 -0500

    first commit test-webpack-react-react

---
+++ test-webpack-react-react/react-host/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        Header
+      </header>
+      <main>
+        Content...
+      </main>
+    </div>
+  );
+}
+
+export default App;
